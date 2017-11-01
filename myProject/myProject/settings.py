b3a7020a6b696d7ca9efa8b2c6fe03883b938e46
playceab <lijian@playcarb.com>
2017-11-01 15:41:44 +0800

    add model

--- myProject/myProject/settings.py
+++ myProject/myProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'DatabaseModel',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'myProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ly_database',
+	'USER': 'root',
+	'PASSWORD': '',
+	'HOST': 'localhost',
+	'PORT': '3306',
     }
 }
 
